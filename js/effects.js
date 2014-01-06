d9853db382711d1cb3b4130fe272e901f04eb4c0
Adam Schwartz <aschwartz@hubspot.com>
2014-01-06 01:03:01 -0500

    Pull in cwilso/Audio-Input-Effects with heavy removal/modifications

---
+++ js/effects.js
@@ -0,0 +1,132 @@
+var audioContext = new AudioContext();
+var audioInput = null,
+    realAudioInput = null,
+    effectInput = null,
+    wetGain = null,
+    dryGain = null,
+    outputMix = null,
+    currentEffectNode = null,
+    reverbBuffer = null,
+    dtime = null,
+    dregen = null,
+    lfo = null,
+    cspeed = null,
+    cdelay = null,
+    cdepth = null,
+    scspeed = null,
+    scldelay = null,
+    scrdelay = null,
+    scldepth = null,
+    scrdepth = null,
+    fldelay = null,
+    flspeed = null,
+    fldepth = null,
+    flfb = null,
+    sflldelay = null,
+    sflrdelay = null,
+    sflspeed = null,
+    sflldepth = null,
+    sflrdepth = null,
+    sfllfb = null,
+    sflrfb = null,
+    rmod = null,
+    mddelay = null,
+    mddepth = null,
+    mdspeed = null,
+    lplfo = null,
+    lplfodepth = null,
+    lplfofilter = null,
+    awFollower = null,
+    awDepth = null,
+    awFilter = null,
+    ngFollower = null,
+    ngGate = null;
+
+
+var rafID = null;
+var analyser1;
+var analyserView1;
+
+function convertToMono( input ) {
+    var splitter = audioContext.createChannelSplitter(2);
+    var merger = audioContext.createChannelMerger(2);
+
+    input.connect( splitter );
+    splitter.connect( merger, 0, 0 );
+    splitter.connect( merger, 0, 1 );
+    return merger;
+}
+
+window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame;
+window.cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame;
+
+function cancelAnalyserUpdates() {
+    window.cancelAnimationFrame( rafID );
+    rafID = null;
+}
+
+function updateAnalysers(time) {
+    analyserView1.doFrequencyAnalysis( analyser1 );
+
+    rafID = window.requestAnimationFrame( updateAnalysers );
+}
+
+var lpInputFilter=null;
+
+// this is ONLY because we have massive feedback without filtering out
+// the top end in live speaker scenarios.
+function createLPInputFilter(output) {
+    lpInputFilter = audioContext.createBiquadFilter();
+    lpInputFilter.frequency.value = 2048;
+    return lpInputFilter;
+}
+
+var useFeedbackReduction = true;
+
+function gotStream(stream) {
+    var input = audioContext.createMediaStreamSource(stream);
+
+    audioInput = convertToMono(input);
+
+    if (useFeedbackReduction) {
+        audioInput.connect( createLPInputFilter() );
+        audioInput = lpInputFilter;
+    }
+
+    // create mix gain nodes
+    outputMix = audioContext.createGain();
+    dryGain = audioContext.createGain();
+    wetGain = audioContext.createGain();
+    effectInput = audioContext.createGain();
+    audioInput.connect(dryGain);
+    audioInput.connect(analyser1);
+    audioInput.connect(effectInput);
+    dryGain.connect(outputMix);
+    wetGain.connect(outputMix);
+    outputMix.connect(audioContext.destination);
+    updateAnalysers();
+}
+
+function initAudio() {
+    o3djs.require('o3djs.shader');
+
+    analyser1 = audioContext.createAnalyser();
+    window.analyser1 = analyser1;
+    analyser1.fftSize = 1024;
+
+    analyserView1 = new AnalyserView('view1');
+    analyserView1.initByteBuffer( analyser1 );
+
+    if (!navigator.getUserMedia)
+        navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    if (!navigator.getUserMedia)
+        return(alert('Error: getUserMedia not supported!'));
+
+    navigator.getUserMedia({audio:true}, gotStream, function(e) {
+        alert('Error getting audio');
+        console.log(e);
+    });
+}
+
+window.addEventListener('load', initAudio );
